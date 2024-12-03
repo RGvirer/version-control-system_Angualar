f5f3b3230447966debd635bb2447dc91caf5d646
RGvirer <rgv1198@gmail.com>
2024-12-04 00:05:47 +0200

    Add repositoryArr in user

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,6 +1,5 @@
 // models/user.model.ts
 import { Repository } from './repository.model';
-import { Version } from './version.model';
 
 export interface User {
   userId: number;
@@ -9,5 +8,4 @@ export interface User {
   createdAt: Date;
   passwordHash: string;
   repositories: Repository[];
-  versions: Version[];
 }
